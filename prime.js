2e63c801a0f57488450ffd7694f7888bc10858c5
ayushdwivedee <ayushbhartiya1494927@gmail.com>
2023-10-06 18:10:17 +0530

    Prime no. code added

--- prime.js
+++ prime.js
@@ -1,1 +1,13 @@
-console.log("hello")
+let num=5;
+let count=0;
+for(let i=1;i<=num;i++){
+    if(num%i==0){
+        count++
+    }
+}
+if(count>0){
+    console.log("It is a prime no.")
+}
+else{
+    console.log("It is not a prime no.")
+}
